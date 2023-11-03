7d2bce6da657e8f6ea7ba655e14c2fe671ee6dbb
XianYuEthan <116093130+XianYuEthan@users.noreply.github.com>
2023-11-03 12:03:31 +0800

    Update lat_controller.cc

--- modules/control/controller/lat_controller.cc
+++ modules/control/controller/lat_controller.cc
@@ -743,7 +743,7 @@ void LatController::UpdateMatrixCompound() {
 
 double LatController::ComputeFeedForward(double ref_curvature) const {
   const double kv =
-      l_r*m/(2*cf_*(lf_+lr_))-lf_*m/(2*cr_*(lf_+lr_))
+      l_r*mass/(2*cf_*(lf_+lr_))-lf_*mass/(2*cr_*(lf_+lr_))
 
   // Calculate the feedforward term of the lateral controller; then change it
   // from rad to %
@@ -758,7 +758,7 @@ double LatController::ComputeFeedForward(double ref_curvature) const {
         (wheelbase_*ref_curvature+ kv*v^2*ref_curvature -
          matrix_k_(0,2)*
              (lr_/2/cf_ -
-              lr_/2/cr_*m*v^2*ref_curvature/wheelbase_)) *
+              lr_/2/cr_*mass*v^2*ref_curvature/wheelbase_)) *
         180 / M_PI * steer_ratio_ / steer_single_direction_max_degree_ * 100;
   }
 
